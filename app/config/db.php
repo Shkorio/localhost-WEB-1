3bfd450237b9785997e6840bf20ab7184a74594e
SHKORIO DMITRY <shkorkodima@gmail.com>
2022-11-29 10:07:49 +1000

    wwww

---
+++ app/config/db.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+return [
+        'dbname' => 'db1135',
+        'user' => 'root',
+        'password' => '',
+        'host' => '127.0.0.1',
+        'port'=> 3006,
+        'driver' => 'pdo_mysql',
+];
